85147d34b1c231cc28bd9d01bb0393e61958e58d
samritbasnet <hereiamsamrit7@outlook.com>
2024-02-28 11:50:28 -0500

    added book card component and user icon

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -21,8 +21,30 @@ const Hero = () => {
           alt='Hero Image'
         />
       </div>
-      <div>
-        <h1 className="  text-4xl font-bold text-yellow-900 mt-8">Popular Book this week</h1>
+      <div className='text-center'> 
+        <h1 className="  text-4xl font-bold text-brown-300 mt-8">Latest Addition</h1>
+      </div>
+
+      {/* Book Card */}
+      <div className="w-full max-w-sm bg-black border border-white-200 rounded-lg shadow dark:bg-gray-400 dark:border-gray-300">
+        <a href="#">
+          <img className="p-8 rounded-t-xl" src="https://marketplace.canva.com/EAD7WmQzMUQ/2/0/1003w/canva-moon-photo-thriller-mystery-book-cover-QE7uv7YSwDQ.jpg" alt="product image" />
+        </a>
+        <div className="px-5 pb-5">
+          <a href="#">
+            <h5 className="text-xl font-semibold tracking-tight text-black-900 dark:text-black">Breaking the Oath</h5>
+          </a>
+          <div className="flex items-center mt-2.5 mb-5">
+            <div className="flex items-center space-x-1 rtl:space-x-reverse">
+              ★★★★
+            </div>
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">4.0</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+          </div>
+        </div>
       </div>
     </div>
   );
