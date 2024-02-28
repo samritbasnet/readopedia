c9e1427c6594f673701228199debaa0b748e97c3
samritbasnet <hereiamsamrit7@outlook.com>
2024-02-28 01:17:26 -0500

    few tweaks

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,33 +1,31 @@
-
 import React from 'react';
+
 const Hero = () => {
   return (
     <div className='max-w-[1640px] mx-auto p-4'>
-      <div className='max-h-[500px] relative'>
+      <div className='relative overflow-hidden rounded-lg shadow-xl'>
         {/* Overlay */}
-        <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
-          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-            Immerse <span className='text-orange-500'></span>
-          </h1>
-          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-            Engage <span className='text-orange-500'></span>
-          </h1>
-          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-            <span className='text-orange-500'>Escape</span> Repeat
-
-          </h1>
+        <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+          <div className='text-center'>
+            <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-bold'>
+              Immerse <span className='text-orange-500'>|</span> Engage <span className='text-orange-500'>|</span> Escape
+            </h1>
+            <p className='text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-300 mt-4'>
+              Discover your next adventure
+            </p>
+          </div>
         </div>
         <img
-          className='w-full max-h-[500px] object-cover'
+          className='w-full h-auto object-cover max-h-[450px]' // Adjusted max-height for slightly smaller image
           src='https://images.pexels.com/photos/2908984/pexels-photo-2908984.jpeg?cs=srgb&dl=pexels-stanislav-kondratiev-2908984.jpg&fm=jpg'
-          alt='/'
+          alt='Hero Image'
         />
       </div>
       <div>
-      <h1 className="text-4xl font-bold text-gray-900 mt-8">Popular Book this week</h1>
-
+        <h1 className="  text-4xl font-bold text-yellow-900 mt-8">Popular Book this week</h1>
       </div>
     </div>
   );
 };
+
 export default Hero;
